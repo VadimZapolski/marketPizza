25a34bcb9a2cdb7aaf1cc1960d7f9403734c2040
Vadim <unistars@mail.ru>
2021-07-15 17:29:07 +0300

    5 commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import Header from "./Component/Header";
-import Content from "./Component/Content";
+import Home from "./Pages/Home";
+import {Route} from "react-router-dom";
+import CartPay from "./Pages/CartPay";
 
 
 function App() {
     return (
         <div>
             <div className="wrapper">
-                <Header />
-                <Content/>
+                <Header/>
+                <div className="content">
+                    <Route path="/" component={Home} exact />
+                    <Route path="/cart" component={CartPay} exact />
+                </div>
             </div>
         </div>
     );
---
+++ src/Pages/CartPay.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function  CartPay () {
+    return (
+        <div>
+            <h1>Корзина</h1>
+        </div>
+    )
+}
+
+export default CartPay
