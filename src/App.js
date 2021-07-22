6cd9763060ebf78663cb4156beacd6f01162ba9a
Vadim <unistars@mail.ru>
2021-07-22 14:47:46 +0300

    6 commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Header from "./Component/Header";
 import Home from "./Pages/Home";
 import {Route} from "react-router-dom";
 import CartPay from "./Pages/CartPay";
+import axios from "axios";
 
 
 function App() {
+
+   const [pizzas , setPizzas] = useState([])
+
+    useEffect(() => {
+        axios.get('http://localhost:3000/db.json')
+            .then(({data}) => {
+            setPizzas(data.pizzas)
+        })
+    } , [])
+
+
     return (
         <div>
             <div className="wrapper">
                 <Header/>
                 <div className="content">
-                    <Route path="/" component={Home} exact />
+                    <Route path="/" render={() =>  <Home pizzas={pizzas}  />}  exact />
                     <Route path="/cart" component={CartPay} exact />
                 </div>
             </div>
